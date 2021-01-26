5cafa4a983bb94b1963e8eff3fa85001ef7b421d
Wade Pate <wadejp8@gmail.com>
2021-01-26 16:59:26 -0700

    Completed tasks

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bc = require(`./controllers/books_controller.js`)
+
+const app = express()
+
+app.use(express.json())
+
+app.get(`/api/books`, bc.read)
+app.post(`/api/books`, bc.create)
+app.put(`/api/books/:id`, bc.update)
+app.delete(`/api/books/:id`, bc.delete)
+
+
+const port = 4000
+app.listen(port, () => {
+  console.log(`Hi! I'm your server! This is exciting, I'm currently on port ${port}`)
+})
+
+
+
+// const express = require('express');
+// const bc = require('./controllers/books_controller.js');
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.static(__dirname + '/../build'))
+
+// app.get('/api/books', bc.read);
+// app.post('/api/books', bc.create);
+// app.put('/api/books/:id', bc.update);
+// app.delete('/api/books/:id', bc.delete);
+
+// const port = 4000;
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}`);
+// });
